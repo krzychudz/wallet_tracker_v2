ddb2e21c19eb811ca118c0dc9a1e0736c1bdefc9
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2022-10-25 20:36:29 +0200

    Provide account cubit to account view

--- lib/features/add_account/presentation/pages/add_account_screen.dart
+++ lib/features/add_account/presentation/pages/add_account_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:wallet_tracker_v2/features/add_account/presentation/cubit/add_account_cubit.dart';
 import 'package:wallet_tracker_v2/features/add_account/presentation/pages/add_account_view.dart';
 
 class AddAccountScreen extends StatelessWidget {
@@ -6,6 +9,9 @@ class AddAccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AddAccountView();
+    return BlocProvider(
+      create: (context) => Modular.get<AddAccountCubit>(),
+      child: const AddAccountView(),
+    );
   }
 }
