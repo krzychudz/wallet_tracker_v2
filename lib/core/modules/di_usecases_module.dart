65c9cb1a6380b6094ef3b1f0664607105e798938
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2022-11-05 19:35:18 +0100

    Move accounts watch to usecase

--- lib/core/modules/di_usecases_module.dart
+++ lib/core/modules/di_usecases_module.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:wallet_tracker_v2/core/domain/usecases/account/add_account.dart';
+import 'package:wallet_tracker_v2/core/domain/usecases/account/watch_accounts.dart';
 
 class DIUseCasesModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.lazySingleton<AddAccount>((i) => AddAccount(i()), export: true),
+        Bind.lazySingleton<AddAccount>(
+          (i) => AddAccount(i()),
+          export: true,
+        ),
+        Bind.lazySingleton<WatchAccounts>((i) => WatchAccounts(i()),
+            export: true),
       ];
 }
--- lib/features/dashboard/presentation/cubit/dashboard_cubit.dart
+++ lib/features/dashboard/presentation/cubit/dashboard_cubit.dart
@@ -1,17 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wallet_tracker_v2/core/domain/repositories/account_repository_interface.dart';
+import 'package:wallet_tracker_v2/core/data/models/account/account.dart';
+import 'package:wallet_tracker_v2/core/domain/usecases/account/watch_accounts.dart';
 import 'package:wallet_tracker_v2/features/dashboard/presentation/cubit/dashboard_state.dart';
 
 class DashboardCubit extends Cubit<DashboardState> {
   DashboardCubit(
-    this._accountRepository,
+    WatchAccounts watchAccounts,
   ) : super(DashboardState()) {
-    print("HERE CREATE");
-
-    _accountRepository.watchAll().listen((accounts) {
+    _accountRepositorySubscription = watchAccounts().listen((accounts) {
       emit(state.copyWith(accounts: accounts));
     });
   }
 
-  final AccountRepositoryInterface _accountRepository;
+  late StreamSubscription<List<Account>> _accountRepositorySubscription;
+
+  @override
+  Future<void> close() async {
+    _accountRepositorySubscription.cancel();
+    super.close();
+  }
 }
