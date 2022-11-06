ea3eefcf5e4702251058921e870d226faf102ed4
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2022-11-06 14:53:08 +0100

    Fix issue with getting balance from db

--- lib/core/extensions/models/account/account_table_data.dart
+++ lib/core/extensions/models/account/account_table_data.dart
@@ -3,7 +3,12 @@ import 'package:wallet_tracker_v2/core/database/database.dart' hide Account;
 
 extension AccountTableDataExt on AccountsTableData {
   Account toAccount() {
-    return Account(id: id, name: name, currencyCode: currencyCode);
+    return Account(
+      id: id,
+      name: name,
+      currencyCode: currencyCode,
+      balance: balance,
+    );
   }
 }
 
