4b065be9ad28ad53b3f11140c659b11f994aaa3d
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2022-11-11 12:22:24 +0100

    Add empty placeholder to accounts list

--- lib/features/dashboard/presentation/pages/dashboard/dashboard_view.dart
+++ lib/features/dashboard/presentation/pages/dashboard/dashboard_view.dart
@@ -4,11 +4,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:wallet_tracker_v2/core/data/models/account/account.dart';
 import 'package:wallet_tracker_v2/core/widgets/app_bar/app_bar.dart';
+import 'package:wallet_tracker_v2/core/widgets/progress_indicator/progress_indicator.dart';
 import 'package:wallet_tracker_v2/features/account_details/account_details_module.dart';
 import 'package:wallet_tracker_v2/features/dashboard/presentation/cubit/dashboard_cubit.dart';
 import 'package:wallet_tracker_v2/features/dashboard/presentation/cubit/dashboard_state.dart';
 import 'package:wallet_tracker_v2/features/dashboard/presentation/widgets/dashboard/account_tile.dart';
 import 'package:wallet_tracker_v2/features/dashboard/presentation/widgets/dashboard/accounts_header.dart';
+import 'package:wallet_tracker_v2/features/dashboard/presentation/widgets/dashboard/empty_account_list_placeholder.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
@@ -23,28 +25,15 @@ class DashboardView extends StatelessWidget {
       body: BlocBuilder<DashboardCubit, DashboardState>(
         builder: (context, state) {
           return state.map(
-            loading: (_) => const ProgressIndicator(),
+            loading: (_) => const DataProgressIndicator(),
             data: (data) {
               final accounts = data.accounts;
-              return accounts.isEmpty
-                  ? const EmptyListPlaceholder()
-                  : AccountsList(accounts: accounts);
+              return AccountsList(accounts: accounts);
             },
           );
         },
       ),
     );
-  }
-}
-
-class EmptyListPlaceholder extends StatelessWidget {
-  const EmptyListPlaceholder({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('You have no accounts'));
   }
 }
 
@@ -64,38 +53,32 @@ class AccountsList extends StatelessWidget {
           padding: EdgeInsets.only(right: 8, left: 8, top: 16),
           sliver: SliverToBoxAdapter(child: AccountsHeader()),
         ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: accounts.length,
-            (context, index) {
-              final account = accounts[index];
-              return Padding(
-                padding: const EdgeInsets.only(right: 8, left: 8, top: 2),
-                child: AccountTile(
-                  account: account,
-                  onTap: () => Modular.to.pushNamed(
-                    AccountDetailsModule.route,
-                    arguments: account.id,
-                  ),
+        accounts.isEmpty
+            ? const SliverPadding(
+                padding: EdgeInsets.only(right: 8, left: 8, top: 2),
+                sliver: SliverToBoxAdapter(
+                  child: EmptyAccountListPlaceholder(),
                 ),
-              );
-            },
-          ),
-        ),
+              )
+            : SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  childCount: accounts.length,
+                  (context, index) {
+                    final account = accounts[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 8, left: 8, top: 2),
+                      child: AccountTile(
+                        account: account,
+                        onTap: () => Modular.to.pushNamed(
+                          AccountDetailsModule.route,
+                          arguments: account.id,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
       ],
-    );
-  }
-}
-
-class ProgressIndicator extends StatelessWidget {
-  const ProgressIndicator({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(color: Colors.black),
     );
   }
 }
---
+++ lib/features/dashboard/presentation/widgets/dashboard/empty_account_list_placeholder.dart
@@ -0,0 +1,27 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class EmptyAccountListPlaceholder extends StatelessWidget {
+  const EmptyAccountListPlaceholder({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 12.0),
+        child: Center(
+          child: Text(
+            'dashboard_no_accounts_placeholder',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyText1,
+          ).tr(),
+        ),
+      ),
+    );
+  }
+}
